0024dd804a820e743486fc2a5be655a00326ba42
voiteshonok <vsvoiteshonok1@gmail.com>
2021-05-27 00:56:30 +0300

    3. templates

--- dj/app/blogengine/blog/views.py
+++ dj/app/blogengine/blog/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def posts_list(request):
-    return HttpResponse("<h1>hello</h1>")
+    n = ["Slava", "Vasa"]
+    return render(request, "blog/index.html", context={'names': n})
