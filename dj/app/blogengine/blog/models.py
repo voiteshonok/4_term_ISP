e2ad8d9e7285eaa22eb08104481e575891724116
voiteshonok <vsvoiteshonok1@gmail.com>
2021-05-27 19:24:20 +0300

    5. added Tag

--- dj/app/blogengine/blog/models.py
+++ dj/app/blogengine/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.fields import CharField
 from django.shortcuts import reverse
 
 # Create your models here.
@@ -7,9 +8,21 @@ class Post(models.Model):
     slug = models.SlugField(max_length=150, unique=True)
     body = models.TextField(blank=True, db_index=True)
     date_pub = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
 
     def get_absolute_url(self):
         return reverse('post_detail_url', kwargs={"slug": self.slug})
 
     def __str__(self):
         return f"{self.title}"
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def get_absolute_url(self):
+        return reverse('tag_detail_url', kwargs={"slug": self.slug})
+
+    def __str__(self):
+        return f'{self.title}'
