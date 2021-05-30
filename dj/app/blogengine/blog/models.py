922fffe94accf423808257f71982d4988187a045
voiteshonok <vsvoiteshonok1@gmail.com>
2021-05-30 16:34:58 +0300

    8. Post create added

--- dj/app/blogengine/blog/models.py
+++ dj/app/blogengine/blog/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from django.db.models.fields import CharField
 from django.shortcuts import reverse
+from django.utils.text import slugify
 
-# Create your models here.
+from time import time
+
+def gen_slug(s):
+    new_slug = slugify(s, allow_unicode=True)
+    return f"{new_slug}-{str(int(time()))}"
+
+
 class Post(models.Model):
     title = models.CharField(max_length=150, db_index=True)
-    slug = models.SlugField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=150, unique=True, blank=True)
     body = models.TextField(blank=True, db_index=True)
     date_pub = models.DateTimeField(auto_now_add=True)
     tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
@@ -15,6 +22,11 @@ class Post(models.Model):
 
     def __str__(self):
         return f'{self.title}'
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = gen_slug(self.title)
+        super().save(*args, **kwargs)
 
 
 class Tag(models.Model):
--- dj/app/blogengine/blog/views.py
+++ dj/app/blogengine/blog/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 from django.views import View
 
 from .models import Post, Tag
-from .utils import ObjectDetailMixin
-from .forms import TagForm
+from .utils import *
+from .forms import TagForm, PostForm
 
 # Create your views here.
 def posts_list(request):
@@ -22,18 +22,20 @@ class TagDetail(ObjectDetailMixin, View):
     template = 'blog/tag_detail.html'
 
 
-class TagCreate(View):
-    def get(self, request):
-        form = TagForm()
-        return render(request, 'blog/tag_create.html', context={'form': form})
+class TagCreate(ObjectCreateMixin, View):
+    form_model = TagForm
+    template = 'blog/tag_create.html'
+    # def get(self, request):
+    #     form = TagForm()
+    #     return render(request, 'blog/tag_create.html', context={'form': form})
 
-    def post(self, request):
-        bound_form = TagForm(request.POST)
+    # def post(self, request):
+    #     bound_form = TagForm(request.POST)
 
-        if bound_form.is_valid():
-            new_tag = bound_form.save()
-            return redirect(new_tag)
-        return render(request, 'blog/tag_create.html', context={'form': bound_form})
+    #     if bound_form.is_valid():
+    #         new_tag = bound_form.save()
+    #         return redirect(new_tag)
+    #     return render(request, 'blog/tag_create.html', context={'form': bound_form})
 
 
 def tags_list(request):
@@ -41,3 +43,17 @@ def tags_list(request):
     return render(request, 'blog/tags_list.html', context={'tags': tags})
 
 
+class PostCreate(ObjectCreateMixin, View):
+    form_model = PostForm
+    template = 'blog/post_create_form.html'
+    # def get(self, request):
+    #     form = PostForm()
+    #     return render(request, 'blog/post_create_form.html', context={'form': form})
+
+    # def post(self, request):
+    #     bound_form = PostForm(request.POST)
+
+    #     if bound_form.is_valid():
+    #         new_post = bound_form.save()
+    #         return redirect(new_post)
+    #     return render(request, 'blog/post_create_form.html', context={'from': bound_form})
