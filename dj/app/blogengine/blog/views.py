7084e113bfd1ef68ce4e7b9ea972f8e635053422
voiteshonok <vsvoiteshonok1@gmail.com>
2021-06-02 02:11:02 +0300

    some search done

--- dj/app/blogengine/blog/views.py
+++ dj/app/blogengine/blog/views.py
@@ -11,9 +11,16 @@ from .forms import TagForm, PostForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
 
+from django.db.models import Q
+
 
 def posts_list(request):
-    posts = Post.objects.all()
+    search_query = request.GET.get('search', '')
+    if search_query:
+        posts = Post.objects.filter(Q(title__icontains=search_query), Q(body__icontains=search_query))
+    else:
+        posts = Post.objects.all()
+
     paginator = Paginator(posts, 2)
 
     page_number = request.GET.get('page', 1)
